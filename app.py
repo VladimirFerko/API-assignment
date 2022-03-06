622ef1fcec10131bd64f36a44e747d3dc18d35e9
Vladimir Ferko <ferkovladimir24@gmail.com>
2022-03-06 17:49:53 +0100

    added templates and created a database

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
-from flask import Flask
-from sqlalchemy import true
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # creating an instance of class Flask
 app = Flask(__name__)
+
+# added database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+
+# initialized the database
+db = SQLAlchemy(app)
+
+# creating a model (SQL table)
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    userId = db.Column(db.Integer, nullable = False, unique = True)
+    title = db.Column(db.String(50), nullable = False)
+    body = db.Column(db.String(200), nullable = False)
+    created_at = db.Column(db.DateTime, default = datetime.utcnow)
+
+    # method for returning an existing models
+    def __repr__(self):
+        return f'title - {self.title}'
+
 
 
 # the first base page
 @app.route('/')
 def base():
-    return "ahoj"
+    return render_template('index.html')
 
 
 
